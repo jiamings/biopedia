4341c5e4f4ff4b42f836c55231ee6522cbfeb800
Jiaming Song <jiaming.tsong@gmail.com>
2014-10-29 22:15:08 +0800

    added charts page. the charts page is on /charts and is very naive.

--- Biopedia.py
+++ Biopedia.py
@@ -57,6 +57,10 @@ def samples(language='en'):
     return render_template('samples.html', language=language, project_name=project_name,
                            sample_list=sample_list, project_fields_name=project_fields_name)
 
+@app.route('/charts', methods=['GET'])
+def charts():
+    return render_template('charts.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
